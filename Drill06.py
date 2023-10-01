54b3c43103af7d0b68f46d82d15d2addd7ed3473
2019182029_이원준 <S159753S@tukorea.ac.kr>
2023-10-01 15:01:39 +0900

    added draw hand when clicked

--- Drill06.py
+++ Drill06.py
@@ -4,6 +4,7 @@ ground_width, ground_height = 1280, 1024
 running = True
 frame = 0
 hand_x, hand_y = [], []
+hands = 0
 cha_x, cha_y = ground_width // 2, ground_height // 2
 
 open_canvas(ground_width, ground_height)
@@ -14,11 +15,17 @@ hand = load_image('hand_arrow.png')
 
 def handle_events() :
     global running
+    global hand_x, hand_y
+    global hands
 
     events = get_events()
     for event in events :
         if(event.type == SDL_QUIT) :
             running = False
+        elif(event.type == SDL_MOUSEBUTTONDOWN) :
+            hand_x.append(event.x)
+            hand_y.append(ground_height - 1 - event.y)
+            hands += 1
         elif(event.type == SDL_KEYDOWN) :
             if(event.key == SDLK_ESCAPE) :
                 running = False
@@ -28,8 +35,11 @@ while(running) :
     clear_canvas()
 
     ground.draw(ground_width // 2, ground_height // 2)
-
+    
     handle_events()
+
+    for i in range(0, hands) :
+        hand.draw(hand_x[i - 1], hand_y[i - 1])
 
     update_canvas()
         
